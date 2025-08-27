1da3ffe707c421fde73bb9e6c91b4741d77cf2b6
Savan168 <persenalsavann@gmail.com>
2025-08-27 16:47:14 +0700

    Portfolio

--- main.js
+++ main.js
@@ -3,11 +3,10 @@ $(document).ready(function(){
   const Interest = ['Networking', 'Linux', 'Tenology','Security','Read books', 'Research']
   const projectsCart = [
     {image:'Projects/DHCP,VLAN.png', title:'DHCP and VLAN Configuration',Link:'Projects/DHCP,VLAN.pkt'},
-    {image:'filedonwload/DHCP.png', title:'Fedora Server: DHCP',Link:'filedonwload/DHCP.pdf'},
-    {image:'filedonwload/DHCP.png', title:'12',Link:'filedonwload/DHCP.pdf'},
-    {image:'filedonwload/DHCP, DNS, Web Server.png', title:'12',Link:'filedonwload/DHCP, DNS, Web Server.pdf'},
-    {image:'filedonwload/Mail Server.png', title:'12',Link:'filedonwload/Mail Server.pdf'},
-    {image:'filedonwload/SSH.png', title:'12',Link:'filedonwload/SSH.pdf'},
+    {image:'filedonwload/DHCP.png', title:'Fedora Server: DHCP',Link:'filedonwload/DHCP.pdf'}, 
+    {image:'filedonwload/DHCP, DNS, Web Server.png', title:'Fidora Server: DHCP DNS and Web Server',Link:'filedonwload/DHCP, DNS, Web Server.pdf'},
+    {image:'filedonwload/Mail Server.png', title:'Fidora Server: Mail Server',Link:'filedonwload/Mail Server.pdf'},
+    {image:'filedonwload/SSH.png', title:'Fidora Server: SSH',Link:'filedonwload/SSH.pdf'},
     {image:'filedonwload/te.png', title:'12',Link:'21'}
   ]
   const VideoCart = [
@@ -38,7 +37,7 @@ $(document).ready(function(){
             <img class="h-full rounded-t-md" src="${val.image}" alt="">
           </div>
           <div class="h-[25%] lg:p-4 p-3"> 
-            <h1 class="lg:mb-3 mb-2">${val.title}</h1>
+            <h1 class="lg:mb-3 mb-2"><b>${val.title}</b></h1>
             <div class="lg:flex lg:flex-row flex flex-col gap-3">
                 <button><a href="">View Configuration LAP</a></button>
                 <button class="">
